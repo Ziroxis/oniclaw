564c705a81318a3ce6f1ac308c0004f4164576a3
Beosti <ziroxismail@gmail.com>
2024-01-21 15:01:25 +0100

    Added oni overdrive ability

---
+++ src/main/java/com/yuanno/oniclawaddon/abilities/oni/OniOverdriveAbility.java
@@ -0,0 +1,109 @@
+package com.yuanno.oniclawaddon.abilities.oni;
+
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.TranslationTextComponent;
+import xyz.pixelatedw.mineminenomi.abilities.gomu.GomuHelper;
+import xyz.pixelatedw.mineminenomi.api.abilities.*;
+import xyz.pixelatedw.mineminenomi.data.entity.ability.AbilityDataCapability;
+import xyz.pixelatedw.mineminenomi.data.entity.entitystats.EntityStatsCapability;
+import xyz.pixelatedw.mineminenomi.init.ModAttributes;
+import xyz.pixelatedw.mineminenomi.init.ModI18n;
+import xyz.pixelatedw.mineminenomi.init.ModParticleEffects;
+import xyz.pixelatedw.mineminenomi.init.ModSounds;
+import xyz.pixelatedw.mineminenomi.wypi.WyHelper;
+
+import java.util.UUID;
+
+public class OniOverdriveAbility extends ContinuousAbility implements IParallelContinuousAbility {
+
+    public static final AbilityCore INSTANCE = new AbilityCore.Builder("Oni Overdrive", AbilityCategory.RACIAL, OniOverdriveAbility::new)
+            .addDescriptionLine("Goes into overdrive, gaining strength, speed and mobility")
+            .build();
+
+    private static final AbilityAttributeModifier JUMP_HEIGHT = new AbilityAttributeModifier(UUID.fromString("c8983d00-b863-11ee-a506-0242ac120002"), INSTANCE, "Oni Overdrive Jump Modifier", 5, AttributeModifier.Operation.ADDITION);
+    private static final AbilityAttributeModifier STRENGTH_MODIFIER = new AbilityAttributeModifier(UUID.fromString("cdeefe24-b863-11ee-a506-0242ac120002"), INSTANCE, "Oni Overdrive Attack Damage Modifier", 4, AttributeModifier.Operation.ADDITION);
+    private static final AbilityAttributeModifier STEP_HEIGHT = new AbilityAttributeModifier(UUID.fromString("d07c4ab6-b863-11ee-a506-0242ac120002"), INSTANCE, "Oni Overdrive Step Height Modifier", 1, AttributeModifier.Operation.ADDITION);
+
+    private boolean prevSprintValue = false;
+
+    public OniOverdriveAbility(AbilityCore abilityCore)
+    {
+        super(abilityCore);
+        this.setThreshold(40);
+        this.setMaxCooldown(125);
+
+        this.onStartContinuityEvent = this::onStartContinuity;
+        this.duringContinuityEvent = this::duringContinuity;
+        this.afterContinuityStopEvent = this::afterContinuityStopEvent;
+
+    }
+
+    private boolean onStartContinuity(PlayerEntity player)
+    {
+
+        player.getAttribute(ModAttributes.STEP_HEIGHT.get()).addTransientModifier(STEP_HEIGHT);
+        player.getAttribute(ModAttributes.JUMP_HEIGHT.get()).addTransientModifier(JUMP_HEIGHT);
+        player.getAttribute(Attributes.ATTACK_SPEED).addTransientModifier(STRENGTH_MODIFIER);
+        player.getAttribute(ModAttributes.PUNCH_DAMAGE.get()).addTransientModifier(STRENGTH_MODIFIER);
+
+        this.prevSprintValue = player.isSprinting();
+        return true;
+    }
+
+    private void duringContinuity(PlayerEntity player, int passiveTimer)
+    {
+        if (passiveTimer % 10 == 0)
+        {
+            WyHelper.spawnParticleEffect(ModParticleEffects.GEAR_SECOND.get(), player, player.getX(), player.getY(), player.getZ());
+        }
+
+        if (player.isSprinting())
+        {
+            if (!prevSprintValue)
+                player.level.playSound(null, player.blockPosition(), ModSounds.TELEPORT_SFX.get(), SoundCategory.PLAYERS, 0.5f, 1);
+        }
+        else
+        {
+            prevSprintValue = false;
+            return;
+        }
+
+        float maxSpeed = 2.2F;
+
+        Vector3d vec = player.getLookAngle();
+        if (player.isOnGround())
+        {
+            player.setDeltaMovement((vec.x * maxSpeed), player.getDeltaMovement().y, (vec.z * maxSpeed));
+        }
+        else
+        {
+            player.setDeltaMovement((vec.x * maxSpeed * 0.5F), player.getDeltaMovement().y, (vec.z * maxSpeed * 0.5F));
+        }
+
+        this.prevSprintValue = player.isSprinting();
+        player.hurtMarked = true;
+    }
+
+    private void afterContinuityStopEvent(PlayerEntity player)
+    {
+        int cooldown = (int) Math.round(this.continueTime / 30.0);
+        this.setMaxCooldown(1 + cooldown);
+        if (this.continueTime > this.getThreshold() / 1.425 && EntityStatsCapability.get(player).getDoriki() < 2000)
+        {
+            player.addEffect(new EffectInstance(Effects.HUNGER, 600, 3, true, true));
+            player.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 200, 1, true, true));
+        }
+
+        player.getAttribute(ModAttributes.STEP_HEIGHT.get()).removeModifier(STEP_HEIGHT);
+        player.getAttribute(ModAttributes.JUMP_HEIGHT.get()).removeModifier(JUMP_HEIGHT);
+        player.getAttribute(Attributes.ATTACK_SPEED).removeModifier(STRENGTH_MODIFIER);
+        player.getAttribute(ModAttributes.PUNCH_DAMAGE.get()).removeModifier(STRENGTH_MODIFIER);
+    }
+}
--- src/main/java/com/yuanno/oniclawaddon/init/ModAbilities.java
+++ src/main/java/com/yuanno/oniclawaddon/init/ModAbilities.java
@@ -4,6 +4,7 @@ import com.yuanno.oniclawaddon.abilities.dragonclaw.FlamingSlashAbility;
 import com.yuanno.oniclawaddon.abilities.dragonclaw.GrabAbility;
 import com.yuanno.oniclawaddon.abilities.dragonclaw.RisingSunAbility;
 import com.yuanno.oniclawaddon.abilities.dragonclaw.TalonsAbility;
+import com.yuanno.oniclawaddon.abilities.oni.OniOverdriveAbility;
 import net.minecraftforge.eventbus.api.IEventBus;
 import xyz.pixelatedw.mineminenomi.api.abilities.AbilityCore;
 import xyz.pixelatedw.mineminenomi.api.enums.AbilityCommandGroup;
@@ -15,6 +16,7 @@ import java.util.Objects;
 public class ModAbilities {
 
     public static final AbilityCore[] DRAGONCLAW_ABILITIES = new AbilityCore[] {TalonsAbility.INSTANCE, GrabAbility.INSTANCE, RisingSunAbility.INSTANCE, FlamingSlashAbility.INSTANCE};
+    public static final AbilityCore[] ONI_ABILITIES = new AbilityCore[] {OniOverdriveAbility.INSTANCE};
 
     private static void registerAbilities(AbilityCore[] abilities)
     {
@@ -26,6 +28,10 @@ public class ModAbilities {
         registerAbilities(DRAGONCLAW_ABILITIES);
         AbilityCommandGroup.create("DRAGONCLAW", () -> {
             return DRAGONCLAW_ABILITIES;
+        });
+        registerAbilities(ONI_ABILITIES);
+        AbilityCommandGroup.create("ONI", () -> {
+            return ONI_ABILITIES;
         });
     }
 }
